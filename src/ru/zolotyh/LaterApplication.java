1a46dba059907c578ef31c0c075d3112bf955322
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2024-08-22 16:41:43 +0300

    Добавить стартовый класс и тестовый контроллер

---
+++ src/ru/zolotyh/LaterApplication.java
@@ -0,0 +1,11 @@
+package ru.zolotyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LaterApplication.class, args);
+    }
+}
